22c28763f709abd52dd503766b4feb67ad43b9d7
whimzhu <zhucaicool@sina.com>
2022-07-06 17:11:09 +0800

    增加ts实例

---
+++ typeScript/index.ts
@@ -0,0 +1,16 @@
+interface Identities<V, M> {
+  value: V,
+  message: M
+}
+
+function identity<T, U>(value: T, message: U): Identities<T, U> {
+  console.log(value + ": " + typeof (value));
+  console.log(message + ": " + typeof (message));
+  let identities: Identities<T, U> = {
+    value,
+    message
+  };
+  return identities;
+}
+
+console.log(identity(68, "Semlinker"));
